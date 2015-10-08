a1dc7d091b9bd462f8d3cdc1f11a3aafe8b68554
unknown <arnaud.denagbe@DGB-DENAGBE.emea.group.atlascopco.com>
2015-10-08 21:20:12 +0200

    my change

--- de/xyr/reget/DownloadChooseDialog.java
+++ de/xyr/reget/DownloadChooseDialog.java
@@ -1,9 +1,11 @@
 package de.xyr.reget;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
 import javax.swing.*;
+
 /**
  * DownloadChooseDialog.java
  *
@@ -12,38 +14,29 @@ import javax.swing.*;
  *
  * @version 1.0
  */
-public class DownloadChooseDialog extends JDialog
-{
-	// little change
-    private Vector urls;
+public class DownloadChooseDialog extends JDialog {
+	private Vector urls;
 
-    public DownloadChooseDialog(Frame parent, File[] files) throws Exception
-    {
-	super(parent, "Downloads selector", true);
-	
-	urls = new Vector();
-	for(int i = 0; i < files.length; i++)
-	{
-	    String filename = files[i].getAbsolutePath();
-	    BufferedReader in = new BufferedReader(new FileReader(filename));
-	    for(String line; (line = in.readLine()) != null; )
-	    {
-		if (line.length() >= 4 && line.substring(0, 4).toLowerCase().equals("http"))
-		{
-		    urls.add(line);
-		};
-	    }
-	    in.close();
+	public DownloadChooseDialog(Frame parent, File[] files) throws Exception {
+		super(parent, "Downloads selector", true);
+
+		urls = new Vector();
+		for (int i = 0; i < files.length; i++) {
+			String filename = files[i].getAbsolutePath();
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			for (String line; (line = in.readLine()) != null;) {
+				if (line.length() >= 4 && line.substring(0, 4).toLowerCase().equals("http")) {
+					urls.add(line);
+				}
+				;
+			}
+			in.close();
+		}
+
+		int i = 0;
 	}
 
-	/*for(int i = 0; i < urls.size(); i++)
-	{
-	    System.out.println(urls.get(i));
-	    }*/
-    }
-
-    public Vector getUrls()
-    {
-	return urls;
-    }
+	public Vector getUrls() {
+		return urls;
+	}
 }// DownloadChooseDialog
